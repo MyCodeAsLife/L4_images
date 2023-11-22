cbb33862f2a8a496ff93d3616799edb68b91ca70
Vladimir <Vladimir@DESKTOP-3QT542Q>
2023-11-22 20:04:05 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_images
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imageCount;
+            int quantityPerRow;
+            int maxRow;
+            int remainder;
+
+            Console.Write("Сколько всего у вас картинок?: ");
+            imageCount = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько картинок должно быть в одном ряду?: ");
+            quantityPerRow = Convert.ToInt32(Console.ReadLine());
+
+            maxRow = imageCount / quantityPerRow;
+            remainder = imageCount % quantityPerRow;
+
+            // Сори за магическое число, просто вставил форматирование текста для красоты
+            Console.WriteLine(new string('-', 45));
+
+            Console.WriteLine($"Полностью заполненых рядов: {maxRow}\n" +
+                              $"Остаток картинок на непоный ряд: {remainder}.\n");
+        }
+    }
+}
