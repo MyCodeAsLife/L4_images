b49d0145b91d06f28998a256cb53a540ea979e34
Vladimir <w112vovan@mail.ru>
2023-11-23 09:53:42 +0500

    Правка 2

--- Program.cs
+++ Program.cs
@@ -23,10 +23,7 @@ namespace L4_images
             countFilledRow = imageCount / quantityPerRow;
             countRemainderImages = imageCount % quantityPerRow;
 
-            // Сори за магическое число, просто вставил форматирование текста для красоты
-            Console.WriteLine(new string('-', 45));
-
-            Console.WriteLine($"Полностью заполненых рядов: {countFilledRow}\n" +
+            Console.WriteLine($"\nПолностью заполненых рядов: {countFilledRow}\n" +
                               $"Остаток картинок на непоный ряд: {countRemainderImages}.\n");
         }
     }
