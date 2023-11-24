63ba0b61920bbf11bc580964f49ab9fd0ccaac7b
Vladimir <w112vovan@mail.ru>
2023-11-24 14:20:41 +0500

    Правка 3

--- Program.cs
+++ Program.cs
@@ -10,18 +10,18 @@ namespace L4_images
     {
         static void Main(string[] args)
         {
-            int imageCount;
-            int quantityPerRow;
+            int imagesCount;
+            int maxImagesInRow;
             int countFilledRow;
             int countRemainderImages;
 
             Console.Write("Сколько всего у вас картинок?: ");
-            imageCount = Convert.ToInt32(Console.ReadLine());
+            imagesCount = Convert.ToInt32(Console.ReadLine());
             Console.Write("Сколько картинок должно быть в одном ряду?: ");
-            quantityPerRow = Convert.ToInt32(Console.ReadLine());
+            maxImagesInRow = Convert.ToInt32(Console.ReadLine());
 
-            countFilledRow = imageCount / quantityPerRow;
-            countRemainderImages = imageCount % quantityPerRow;
+            countFilledRow = imagesCount / maxImagesInRow;
+            countRemainderImages = imagesCount % maxImagesInRow;
 
             Console.WriteLine($"\nПолностью заполненых рядов: {countFilledRow}\n" +
                               $"Остаток картинок на непоный ряд: {countRemainderImages}.\n");
